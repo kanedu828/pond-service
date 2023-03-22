5f9f21182a752ae9a4d531c7a5961438dec82f61
Kane Du <45084706+kanedu828@users.noreply.github.com>
2023-03-22 15:51:54 -0400

    Change CORS from string to boolean

--- src/app.ts
+++ src/app.ts
@@ -32,7 +32,7 @@ app.use(
 // CORS Headers
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', POND_WEB_URL);
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader('Access-Control-Allow-Credentials', true);
   next();
 });
 
